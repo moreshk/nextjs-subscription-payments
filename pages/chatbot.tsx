23c36679722c3ed0fa7271512e6476e8094c4992
viraj-kokane <virajkokane123@gmail.com>
2023-05-26 16:11:22 +0530

    fix type

--- pages/chatbot.tsx
+++ pages/chatbot.tsx
@@ -216,7 +216,7 @@ export default function Chatbot({ user }: { user: User }) {
     }
   };
 
-  const copyToClipboard = (text) => {
+  const copyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text);
     setCopySuccess(true);
     setTimeout(() => {
